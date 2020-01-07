3af411548eeaa407a7292de36e819ccf485b6b82
pacanor <papiasb@gmail.com>
2020-01-07 17:50:43 +0200

    Feature: Added tests for the calculator

---
+++ Day7Calc2Tests/AdditionTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Day7Calc2;
+
+namespace Day7Calc2.Tests
+{
+    [TestClass()]
+    public class AdditionTests
+    {
+        [TestMethod()]
+        public void SumTest()
+        {
+            Assert.AreEqual(5, Calculator.Add(a, b);
+        }
+    }
+}
